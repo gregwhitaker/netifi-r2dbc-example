13bafa8c90d7175ef50453182247438e554a8ec9
gregwhitaker <gwhitake@gmail.com>
2019-11-21 09:49:04 -0800

    Initial Commit

---
+++ product-service/src/main/java/example/service/product/ProductServiceApplication.java
@@ -0,0 +1,8 @@
+package example.service.product;
+
+public class ProductServiceApplication {
+
+    public static void main(String... args) {
+
+    }
+}
---
+++ store-app/src/main/java/example/storeapp/StoreApplication.java
@@ -0,0 +1,8 @@
+package example.storeapp;
+
+public class StoreApplication {
+
+    public static void main(String... args) {
+
+    }
+}
