1577c0f93231277de973afda92897019704d0b52
gregwhitaker <gwhitake@gmail.com>
2019-11-21 10:32:02 -0800

    Doing work

--- product-service/src/main/java/example/service/product/ProductServiceApplication.java
+++ product-service/src/main/java/example/service/product/ProductServiceApplication.java
@@ -15,9 +15,13 @@
  */
 package example.service.product;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ProductServiceApplication {
 
     public static void main(String... args) {
-
+        SpringApplication.run(ProductServiceApplication.class, args);
     }
 }
