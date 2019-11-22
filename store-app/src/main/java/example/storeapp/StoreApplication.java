c854c1d5e3ec7201a71e94bbd339d6f06e8f5cd4
gregwhitaker <gwhitake@gmail.com>
2019-11-22 12:21:46 -0800

    Doing work

--- store-app/src/main/java/example/storeapp/StoreApplication.java
+++ store-app/src/main/java/example/storeapp/StoreApplication.java
@@ -15,9 +15,13 @@
  */
 package example.storeapp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class StoreApplication {
 
     public static void main(String... args) {
-
+        SpringApplication.run(StoreApplication.class, args);
     }
 }
