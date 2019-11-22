f688a47db859290f767e72a8727fe54659d77161
gregwhitaker <gwhitake@gmail.com>
2019-11-22 12:10:06 -0800

    Doing work

--- product-service/src/main/java/example/service/product/data/ProductDao.java
+++ product-service/src/main/java/example/service/product/data/ProductDao.java
@@ -17,19 +17,11 @@ package example.service.product.data;
 
 import example.service.product.protobuf.ProductInfoResponse;
 import io.r2dbc.client.R2dbc;
-import io.r2dbc.pool.ConnectionPool;
-import io.r2dbc.spi.Result;
-import io.r2dbc.spi.Row;
-import io.r2dbc.spi.RowMetadata;
-import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
-
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 @Component
 public class ProductDao {
@@ -39,19 +31,16 @@ public class ProductDao {
     private R2dbc r2dbc;
 
     public Mono<ProductInfoResponse> getProduct(long productId) {
-//        return r2dbc.withHandle(handle -> {
-//            handle.select("SELECT * FROM products WHERE id = $1", productId)
-//                    .mapResult((Function<Result, Publisher<ProductInfoResponse>>) result -> result.map((row, rowMetadata) -> {
-//                        ProductInfoResponse response = ProductInfoResponse.newBuilder()
-//                                .setProductId(row.get("id", Long.class))
-//                                .setActive(row.get("active", Boolean.class))
-//                                .setShortName(row.get("short_name", String.class))
-//                                .setLongName(row.get("long_name", String.class))
-//                                .setDescription(row.get("description", String.class))
-//                                .build();
-//
-//                        return response;
-//                    })
-//        });
+        return Mono.defer(() -> r2dbc.withHandle(handle ->
+                handle.select("SELECT * FROM products WHERE id = $1", productId)
+                    .mapRow((row, rowMetadata) -> ProductInfoResponse.newBuilder()
+                        .setProductId(row.get("id", Long.class))
+                        .setActive(row.get("active", Boolean.class))
+                        .setShortName(row.get("short_name", String.class))
+                        .setLongName(row.get("long_name", String.class))
+                        .setDescription(row.get("description", String.class))
+                        .build())
+                    .next())
+                .next());
     }
 }
