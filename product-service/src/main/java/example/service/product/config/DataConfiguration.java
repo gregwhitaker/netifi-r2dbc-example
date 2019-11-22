0ec453b0a01a90c6eef5ce1113c7fc618bd0d2ea
gregwhitaker <gwhitake@gmail.com>
2019-11-22 11:42:09 -0800

    Doing work

--- product-service/src/main/java/example/service/product/config/DataConfiguration.java
+++ product-service/src/main/java/example/service/product/config/DataConfiguration.java
@@ -20,7 +20,6 @@ import io.r2dbc.client.R2dbc;
 import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactoryOptions;
-import io.r2dbc.spi.Option;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
